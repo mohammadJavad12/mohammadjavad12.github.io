a03eee51ad42e8c91c9a9f19e2f35f0e7196cf99
Mohammadjavad Haghbeigi <80960346+mohammadJavad12@users.noreply.github.com>
2025-06-18 09:14:05 +0330

    Update app.py

--- app.py
+++ app.py
@@ -37,4 +37,5 @@ def handle_disconnect():
     emit('user_list', list(users.values()), broadcast=True)
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
     socketio.run(app, host='0.0.0.0', port=port,allow_unsafe_werkzeug=True)
