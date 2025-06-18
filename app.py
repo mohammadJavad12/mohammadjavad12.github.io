ec30285704bc1bd34c1f5a8ceb57b9465a503ca7
Mohammadjavad Haghbeigi <80960346+mohammadJavad12@users.noreply.github.com>
2025-06-18 09:16:06 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
